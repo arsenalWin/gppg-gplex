d6a73a9f714fe86dcb7a6b44f02edea9cffeee45
stern <stern1991@outlook.com>
2018-09-03 21:50:31 +0800

    项目初始化

---
+++ gppg-gplex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gppg_gplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
